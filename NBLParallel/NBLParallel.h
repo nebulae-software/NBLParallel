305c842f224cb65007adba3ca15f228bd0fb8b69
Jorge Galrito <jorge.galrito@me.com>
2019-01-03 01:04:55 +0000

    Initial Commit

---
+++ NBLParallel/NBLParallel.h
@@ -0,0 +1,19 @@
+//
+//  NBLParallel.h
+//  NBLParallel
+//
+//  Created by Jorge Galrito on 03/01/2019.
+//  Copyright © 2019 Nebulae Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NBLParallel.
+FOUNDATION_EXPORT double NBLParallelVersionNumber;
+
+//! Project version string for NBLParallel.
+FOUNDATION_EXPORT const unsigned char NBLParallelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NBLParallel/PublicHeader.h>
+
+
